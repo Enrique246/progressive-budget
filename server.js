e1fa22f025d43c8569ea251114a1f300df6a3d65
Henri <ecch9210@gmail.com>
2021-08-04 22:37:53 -0500

    MongoDB estabilshment

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config()
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
@@ -15,13 +16,19 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false,
 });
+mongoose.connection.on('connected', () =>
+  console.log('Connected to MongoDB Endpoint')
+);
 
+mongoose.connection.on('error', (err) =>
+  console.log(`Mongoose default connection error: ${err}`)
+);
 // routes
 app.use(require("./routes/api.js"));
 
